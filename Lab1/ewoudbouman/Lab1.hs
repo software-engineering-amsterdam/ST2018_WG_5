45030d4a2394f2c6ee9562f23ca79aa656ff16ff
ewoudbouman <boumanewoud@gmail.com>
2018-09-06 18:13:24 +0200

    started cleaning it up

--- Lab1/ewoudbouman/Lab1.hs
+++ Lab1/ewoudbouman/Lab1.hs
@@ -10,16 +10,14 @@ prime n = n > 1 && all (\ x -> rem n x /= 0) xs
 primes :: [Integer]
 primes = 2 : filter prime [3..] 
 
+-- Note: assumes that Integer n is a prime
 primes' :: Integer -> [Integer]
-primes' n = n : filter prime [(n+1)..] -- CHECKEN i assume n = prime!
+primes' n = n : filter prime [(n+1)..]
 
 infix 1 --> 
 
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
-
-forall :: [a] -> (a -> Bool) -> Bool
-forall = flip all
 
 reversal :: Integer -> Integer
 reversal = read . reverse . show
@@ -29,95 +27,70 @@ data Boy = Matthew | Peter | Jack | Arnold | Carl
 
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
--- test examples
-
-sentence = "Sentences can go " ++ onAndOn
-onAndOn = "on and on " ++ onAndOn
-
-sentences = "Sentences can go on":
-  map (++ " and on") sentences
-
-myall :: (a -> Bool) -> [a] -> Bool
-myall p [] = True
-myall p (x:xs) = p x && myall p xs
-
-list2p :: Eq a => [a] -> a -> Bool
-list2p = flip elem
-
-myallTest :: [Int] -> [Int] -> Bool
-myallTest = \ ys xs -> let p = list2p ys in 
-  all p xs == myall p xs
-
 divide :: Integer -> Integer -> Bool
 divide n m = rem m n == 0
 
 isPrime :: Integer -> Bool
-isPrime n = n > 1 && all (\ d -> not(divide d n))[2..n-1]
-
-isPrime' :: Integer -> Bool
-isPrime' n = all (\ x -> rem n x /= 0) xs
+isPrime n = all (\ x -> rem n x /= 0) xs
   where xs = takeWhile (\ y -> y^2 <= n) [2..]
 
 -- 
 --
 -- 1
 --
+--
+-- Time: 45 min
+-- Most of it was spend fighting with quickcheck
+
 -- exercise 2 from the workshop
 --
 
-lside1 :: Int -> Int
-lside1 n = sum [x ^ 2 | x <- [1..n]]
+lsidea :: Int -> Int
+lsidea n = sum [x ^ 2 | x <- [1..n]]
 
-rside1 :: Int -> Int
-rside1 n = div (n * (n + 1) * (2 * n + 1)) 6
+rsidea :: Int -> Int
+rsidea n = div (n * (n + 1) * (2 * n + 1)) 6
 
--- failes with -2 negative numbers
--- true because 1,2,...
--- https://stackoverflow.com/questions/39291494/only-generate-positive-integers-with-quickcheck
-checkq12 = quickCheck (\ (Positive x) -> lside1 x == rside1 x)
+-- quickcheck
+checkWorkshop2 = quickCheck (\ (Positive x) -> lsidea x == rsidea x)
 
 --
 -- exercise 3 from the workshop
 --
 
-lside2 :: Int -> Int
-lside2 n = sum [x ^ 3 | x <- [1..n]]
+lsideb :: Int -> Int
+lsideb n = sum [x ^ 3 | x <- [1..n]]
 
-rside2 :: Int -> Int
-rside2 n = (div  (n * (n + 1)) 2) ^ 2
+rsideb :: Int -> Int
+rsideb n = div  (n * (n + 1)) 2 ^ 2
 
-checkq22 = quickCheck (\ (Positive x) -> lside2 x == rside2 x)
+checkWorkshop3 = quickCheck (\ (Positive x) -> lsideb x == rsideb x)
 
 --
 -- 2
 --
--- set a = n dan gek ding a = 2^n
+-- Time: 20 min
+-- WIP: missing quickstep verification
 
-left2 :: [a] -> Int
-left2 i = length (subsequences i)
+setA :: [a] -> Int
+setA i = length (subsequences i)
 
-right2 :: [a] -> Int
-right2 i = 2 ^ (length i)
-
--- quichcheck, problem is grote nummers?
+subSets :: [a] -> Int
+subSets i = 2 ^ length i
 
 --
 -- 3
 --
--- Redo exercise 5 of Workshop 1 by replacing sets by lists, and testing the property for integer lists of the form [1..n].
-perms :: [a] ->[[a]]
-perms [] = [[]]
-perms (x:xs) = concat (map (insrt x) (perms xs)) where
-  insrt x [] = [[x]]
-  insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
+-- Time: 30 min
+--
+-- Note that the proof is dodgy because of the bigly numbers
 
--- copy pasta
--- https://stackoverflow.com/questions/40097116/get-all-permutations-of-a-list-in-haskell
-permutate' :: (Eq a) => [a] -> [[a]]
-permutate' [] = [[]]
-permutate' l = [a:x | a <- l, x <- (permutate' $ filter (\x -> x /= a) l)]
+numberPerm, numberObjects :: Int -> Int
+numberPerm n = length (permutations [1..n])
+numberObjects n = product [1..n]
 
--- bs is checken if list is same? perhaps factorial?
+checkWorkshop5 = quickCheckResult (\ (Positive x) -> (x < 10) --> numberPerm x == numberObjects x)
+
 
 --
 -- 4
@@ -125,12 +98,12 @@ permutate' l = [a:x | a <- l, x <- (permutate' $ filter (\x -> x /= a) l)]
 --reversal :: Integer -> Integer
 --reversal = read . reverse . show
 --testp :: [Ord] => a -> [a]
-q4 = [x | x <- [1..10001], isPrime' x && isPrime' (reversal x)]
+q4 = [x | x <- [1..10001], isPrime x && isPrime (reversal x)]
 
-checkprime = [x | x <- [1..10001], isPrime' x]
-checkprimerev = [x | x <- [1..10001], isPrime' (reversal x)]
+checkprime = [x | x <- [1..10001], isPrime x]
+checkprimerev = [x | x <- [1..10001], isPrime (reversal x)]
 
-check4 = quickCheck (q4 == (intersect checkprime checkprimerev))
+check4 = quickCheck (q4 == intersect checkprime checkprimerev)
 
 --
 -- 5
@@ -142,10 +115,10 @@ primes2 :: [Integer] -> [Integer]
 primes2 xs = tail(xs) ++ filter prime[(last(xs)+1)..]
 
 testq5 ::[Integer] -> Integer
---testq5 xs = isPrime'(sum xs)
+--testq5 xs = isPrime(sum xs)
 --testq5 p (x:xs) = p x && myall p xs
 testq5 xs
-  | isPrime'(sum xs) = sum xs
+  | isPrime(sum xs) = sum xs
   | otherwise = checkFurther
   where checkFurther = testq5 (take 101 (primes2 xs))
 
@@ -160,7 +133,7 @@ testq5 xs
 
 testq6 :: Int -> [Integer]
 testq6 n 
-  | not(isPrime'((product(take n primes)) + 1)) =  take n primes
+  | not(isPrime((product(take n primes)) + 1)) =  take n primes
   | otherwise = checkFurther
   where checkFurther = testq6 (n+1)
 
@@ -201,23 +174,24 @@ isVisa n = False
 --
 -- 8
 --
+-- Time: 60 min
+-- Problem was understanding the problem correctly
+
 accuses :: Boy -> Boy -> Bool
 -- case Matthew
-accuses Matthew x = not(x == Carl) && not(x == Matthew)
+accuses Matthew x = (x /= Carl) && (x /= Matthew)
 -- case Peter
 accuses Peter x = (x == Matthew) || (x == Jack)
 -- case Jack
 accuses Jack x = not(accuses Matthew x) && not(accuses Peter x)
 -- case Arnold
-accuses Arnold x = (accuses Matthew x) /= (accuses Peter x)
+accuses Arnold x = accuses Matthew x /= accuses Peter x
 -- case Carl
 accuses Carl x = not (accuses Arnold x)
--- otherwise
--- accuses x y = False
 
 accusers :: Boy -> [Boy]
 accusers x = [n | n <- boys, accuses n x]
 
 guilty, honest :: [Boy]
-guilty = [n | n <- boys, (length(accusers n) == 3)]
-honest = accusers (guilty!!0)
+guilty = [n | n <- boys, length (accusers n) == 3]
+honest = accusers (head guilty)
