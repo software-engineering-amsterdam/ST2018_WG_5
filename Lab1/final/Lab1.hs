73b1d2c725e8423c2fdba32efe178b9ab492032c
Michael Peters <m.peters@m2mobi.com>
2018-09-11 09:56:36 +0200

    Fixed the broken final assignments

--- Lab1/final/Lab1.hs
+++ Lab1/final/Lab1.hs
@@ -1,7 +1,28 @@
+import Test.QuickCheck
+import Data.List
+import Data.Char
+import Debug.Trace
+import Test.QuickCheck
+
+prime :: Int -> Bool
+prime n = n > 1 && all (\ x -> rem n x /= 0) xs
+  where xs = takeWhile (\ y -> y^2 <= n) primes
+
+primes :: [Int]
+primes = 2 : filter prime [3..]
+
+reversal :: Int -> Int
+reversal = read . reverse . show
+
+primesFrom :: Int -> [Int]
+primesFrom x = filter prime [x..]
+
+takePrimesFrom :: Int -> Int -> [Int]
+takePrimesFrom amount from = take amount (primesFrom from)
+
 {-------------------------------------------------------
     Assignment 1 part 1
 --------------------------------------------------------}
-
 -- 1^2 + 2^2 + .. + n^2
 sumSquares :: Int -> Int
 sumSquares n = sum [x ^ 2 | x <- [1..n]]
@@ -16,7 +37,6 @@ checkWorkshop2 = quickCheck (\ (Positive x) -> sumSquares x == fastSumSquares x)
 {-------------------------------------------------------
     Assignment 1 part 2
 --------------------------------------------------------}
-
 -- 1^3 + 2^3 + .. + n^3
 lsideb :: Int -> Int
 lsideb n = sum [x ^ 3 | x <- [1..n]]
@@ -26,6 +46,7 @@ rsideb :: Int -> Int
 rsideb n = div  (n * (n + 1)) 2 ^ 2
 
 checkWorkshop3 = quickCheck (\ (Positive x) -> lsideb x == rsideb x)
+
 {-------------------------------------------------------
     Assignment 2
 
@@ -38,7 +59,6 @@ checkWorkshop3 = quickCheck (\ (Positive x) -> lsideb x == rsideb x)
     Of course it is needed that the subsequences functions works as intended,
     but that's is out of scope for this particular test and should be tested in a separate test
 --------------------------------------------------------}
-
 -- |A| = n
 setA :: Int -> Int
 setA i = length (subsequences [1..i])
@@ -61,12 +81,10 @@ checkWorkshop4 = quickCheck (\ (Positive x) -> setA x == subSets x)
     Of course it is needed that the perms functions works as intended,
     but that's is out of scope for this particular test and should be tested in a separate test
 --------------------------------------------------------}
+permLength :: [Int] -> Int
+permLength x = product [1..(length x)]
 
-numberPerm, numberObjects :: Int -> Int
-numberPerm n = length (permutations [1..n])
-numberObjects n = product [1..n]
-
-checkWorkshop5 = quickCheckResult (\ (Positive x) -> (x < 10) --> numberPerm x == numberObjects x)
+checkWorkshopFive = quickCheckResult (\ a -> length (permutations a) == permLength a)
 
 {-------------------------------------------------------
     Assignment 4
@@ -74,16 +92,15 @@ checkWorkshop5 = quickCheckResult (\ (Positive x) -> (x < 10) --> numberPerm x =
     Answers: By creating a list of all the reversable primes and checking if the tests passes for these
     and the tests do not pass for other numbers.
 --------------------------------------------------------}
-
-reversablePrime :: Integer -> [Integer]
+reversablePrime :: Int -> [Int]
 reversablePrime x = [x | prime (reversal x)]
 
-filterReversablePrimes :: [Integer] -> [Integer] -> [Integer]
-filterReversablePrimes (x:xs) res = if x < 10000 
+filterReversablePrimes :: [Int] -> [Int] -> [Int]
+filterReversablePrimes (x:xs) res = if x < 10000
                                     then filterReversablePrimes xs (res ++ (reversablePrime x))
                                     else res
 
-generateReversablePrimes :: [Integer]
+generateReversablePrimes :: [Int]
 generateReversablePrimes = filterReversablePrimes primes []
 
 {-------------------------------------------------------
@@ -92,7 +109,6 @@ generateReversablePrimes = filterReversablePrimes primes []
     Answer: The total is 37447 and covers the primes range of (83 - 677)
     To verify it might be possible to test the prime generator by checking if every number is a prime.
 --------------------------------------------------------}
-
 listOfhundredOnes :: [[Int]]
 listOfhundredOnes = [takePrimesFrom 101 x | x <- [1..]]
 assignment5 = sum (filter (\xs -> prime(sum xs)) listOfhundredOnes !! 1)
@@ -102,7 +118,6 @@ assignment5 = sum (filter (\xs -> prime(sum xs)) listOfhundredOnes !! 1)
 
     Answers: The smalles counter example is 30031
 --------------------------------------------------------}
-
 takePrimes :: Int -> [Int]
 takePrimes x = take x primes
 assignment6 = (filter (\x -> not (prime(x))) [(product (takePrimes x) + 1) | x <- [1..]]) !! 0
@@ -110,15 +125,14 @@ assignment6 = (filter (\x -> not (prime(x))) [(product (takePrimes x) + 1) | x <
 {-------------------------------------------------------
     Assignment 7
 --------------------------------------------------------}
-
 doDouble :: Char -> Char
-doDouble num = if x >= 10 
-               then intToDigit (x-9) 
+doDouble num = if x >= 10
+               then intToDigit (x-9)
                else intToDigit x where x = digitToInt num * 2
 
 decideDouble :: Int -> Char -> Char
-decideDouble n num = if mod n 2 == 0 
-                     then doDouble num 
+decideDouble n num = if mod n 2 == 0
+                     then doDouble num
                      else num
 
 calculateSumDigits :: Int -> [Char] -> [Char] -> [Char]
@@ -131,33 +145,45 @@ luhnMiddle number = map digitToInt x where x = calculateSumDigits 1 (reverse num
 luhnAlgorithm :: [Char] -> Bool
 luhnAlgorithm number = mod (sum (luhnMiddle number) * 9) 10 == 0
 
-luhn :: Integer -> Bool
+luhn :: Int -> Bool
 luhn number = luhnAlgorithm (show number)
 
-masterCheckOne :: Integer -> Bool
+masterCheckOne :: Int -> Bool
 masterCheckOne number = x >= 2221 && x <= 2720 where x = div number 1000000000000
 
-masterCheckTwo :: Integer -> Bool
+masterCheckTwo :: Int -> Bool
 masterCheckTwo number = x >= 51 && x <= 55 where x = div number 100000000000000
-                    
-isMaster :: Integer -> Bool
+
+isMaster :: Int -> Bool
 isMaster number = luhn number && (masterCheckOne number || masterCheckTwo number)
 
-visaCheck :: Integer -> Bool
+visaCheck :: Int -> Bool
 visaCheck number = x == "4" where x = take 1 (show number)
 
-isVisa :: Integer -> Bool
+isVisa :: Int -> Bool
 isVisa number = luhn number && visaCheck number
 
-aeCheck :: Integer -> Bool
+aeCheck :: Int -> Bool
 aeCheck number = x == "34" || x == "37" where x = take 2 (show number)
 
-isAmericanExpress :: Integer -> Bool
+isAmericanExpress :: Int -> Bool
 isAmericanExpress number = luhn number && aeCheck number
+
+-- Some test cases
+testVisa = [(isVisa 4111111111111111), (isVisa 4012888888881881), not (isVisa 4111132111111111), not (isVisa 4012588888881881)]
+testMaster = [(isMaster 5555555555554444), (isMaster 5105105105105100), not (isMaster 5555555553554444), not (isMaster 5105125105105100)]
+testAmerican = [(isAmericanExpress 378282246310005), (isAmericanExpress 371449635398431), (isAmericanExpress 378734493671000),
+                not (isAmericanExpress 378282245310005), not (isAmericanExpress 371449235398431), not (isAmericanExpress 378734494671000)]
 
 {-------------------------------------------------------
     Assignment 8
 --------------------------------------------------------}
+-- contains the different boys present in the story
+data Boy = Matthew | Peter | Jack | Arnold | Carl
+           deriving (Eq,Show)
+
+-- list of  all possible boys in the story
+boys = [Matthew, Peter, Jack, Arnold, Carl]
 
 accuses :: Boy -> Boy -> Bool
 accuses Matthew x = (x /= Carl) && (x /= Matthew)
