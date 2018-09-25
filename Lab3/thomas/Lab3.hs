3d567b8e578ac2f1e2884346355096978397339b
Thomas van der Ham <thomasvanderham@outlook.com>
2018-09-25 14:41:19 +0200

    Finished assignment 4

--- Lab3/thomas/Lab3.hs
+++ Lab3/thomas/Lab3.hs
@@ -29,7 +29,7 @@ equiv :: Form -> Form -> Bool
 equiv a b = all (\ x -> evl x a == evl x b) (allVals b) && all (\ x -> evl x a == evl x b) (allVals a)
 
 -- Assignment 2
--- TODO
+
 
 -- Assignment 3
 
@@ -44,30 +44,74 @@ cnf (Dsj fs) = Dsj (map cnf fs)
 cnf (Neg (Cnj fs)) = Dsj (map (cnf.Neg) fs)
 cnf (Neg (Dsj fs)) = Cnj (map (cnf.Neg) fs) 
 
+toSingleCNF :: Form -> Form
+toSingleCNF x = cnf (nnf (arrowfree x))
+
 toCNF :: [Form] -> [Form]
 toCNF x = map cnf (map nnf (map arrowfree x))
 
 --Assignment 4
+cnjLiftWrapper :: Form -> Form -> Form
+cnjLiftWrapper x y = Cnj [x, y]
+
+dsjLiftWrapper :: Form -> Form -> Form
+dsjLiftWrapper x y = Dsj [x, y]
+
 boundedForm :: Gen Form
 boundedForm = sized form where
     form :: Int -> Gen Form
-    form 1 = return (Prop 1)
-    form n = oneof [return (Prop n), 
+    form n
+        | n > 0 = oneof [return (Prop n), 
                     liftM Neg (form (n-1)),
                     liftM2 Impl (form (n `div` 2)) (form (n `div` 2)),
-                    --liftM Cnj ( [form (n `div` 2)] ++ [form (n `div` 2)] ),
-                    liftM Cnj [ (form (n `div` 2)), (form (n `div` 2)) ]
+                    liftM2 Equiv (form (n `div` 2)) (form (n `div` 2)),
+                    liftM2 cnjLiftWrapper (form (n `div` 2)) (form (n `div` 2)),
+                    liftM2 dsjLiftWrapper (form (n `div` 2)) (form (n `div` 2))
                    ]
+        | otherwise = return (Prop n)
 
--- instance Arbitrary Form where
---     arbitrary = 
---         oneof [return (Prop 1),
---                liftM Neg arbitrary,
---                liftM Cnj arbitrary,
---                --liftM Dsj arbitrary,
---                --liftM2 Impl arbitrary arbitrary,
---                liftM2 Equiv arbitrary arbitrary
---                ]
+-- 
+containsNoArrows :: Form -> Bool
+containsNoArrows (Prop x) = True
+containsNoArrows (Neg f) =  containsNoArrows f
+containsNoArrows (Cnj fs) = all containsNoArrows fs
+containsNoArrows (Dsj fs) = all containsNoArrows fs
+containsNoArrows (Impl f1 f2) = False
+containsNoArrows (Equiv f1 f2) = False
 
-testnow = verboseCheck (forAll boundedForm satisfiable)
--- testnow = generate arbitrary :: IO Form
+noNegativeClauses :: Form -> Bool
+noNegativeClauses (Prop x) = True
+noNegativeClauses (Cnj fs) = all noNegativeClauses fs
+noNegativeClauses (Dsj fs) = all noNegativeClauses fs
+noNegativeClauses (Impl f1 f2) = noNegativeClauses f1 && noNegativeClauses f2
+noNegativeClauses (Equiv f1 f2) = noNegativeClauses f1 && noNegativeClauses f2
+noNegativeClauses (Neg (Cnj _)) = False
+noNegativeClauses (Neg (Dsj _)) = False
+noNegativeClauses (Neg (Impl _ _)) = False
+noNegativeClauses (Neg (Equiv _ _)) = False
+noNegativeClauses (Neg f) =  noNegativeClauses f
+
+areEquivalent :: Form -> Form -> Bool
+areEquivalent x y = equiv x y
+
+noNestedAndOr :: Form -> Bool
+noNestedAndOr (Prop x) = True
+noNestedAndOr (Neg f) =  noNestedAndOr f
+noNestedAndOr (Cnj [_, (Cnj [_])]) = False
+noNestedAndOr (Cnj [(Cnj [_]), _]) = False
+noNestedAndOr (Cnj fs) = all noNestedAndOr fs
+noNestedAndOr (Dsj [_, (Dsj [_])]) = False
+noNestedAndOr (Dsj [(Dsj [_]), _]) = False
+noNestedAndOr (Dsj [_, (Cnj [_])]) = False
+noNestedAndOr (Dsj [(Cnj [_]), _]) = False
+noNestedAndOr (Dsj fs) = all noNestedAndOr fs
+noNestedAndOr (Impl f1 f2) = noNestedAndOr f1 && noNestedAndOr f2
+noNestedAndOr (Equiv f1 f2) = noNestedAndOr f1 && noNestedAndOr f2
+
+checkIsCNF :: Form -> Bool
+checkIsCNF x = containsNoArrows result && noNegativeClauses result &&
+        areEquivalent result x && noNestedAndOr result
+    where result = toSingleCNF x
+
+-- testnow = verboseCheckWith stdArgs {maxSize = 20} (forAll boundedForm satisfiable)
+assignment4 = quickCheckWith stdArgs {maxSize = 20, maxSuccess = 1000} (forAll boundedForm checkIsCNF)
