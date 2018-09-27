d914ee031dd9a6109aa712e229bcd29577c7a804
Thomas van der Ham <thomasvanderham@outlook.com>
2018-09-27 13:16:26 +0200

    Finished lab4 through assignment 4

--- Lab4/thomas/Lab4.hs
+++ Lab4/thomas/Lab4.hs
@@ -2,6 +2,7 @@ module Lab4 where
 
 import Data.List
 import System.Random
+import Control.Monad(liftM, liftM2)
 import Test.QuickCheck
 
 {-- START OF LECTURE CODE --}
@@ -89,24 +90,108 @@ unionSet (Set (x:xs)) set2  =
 ------------------------------------------------------------------------------}
 
 
-
 {------------------------------------------------------------------------------
 
   Assignment 2
 
-  Hours spent: ?
+  Hours spent: 1
 
 ------------------------------------------------------------------------------}
 
+generateRandomInts :: Int -> Int -> Int -> IO [Int]
+generateRandomInts 0 x y = return []
+generateRandomInts n x y = do
+        p <- getStdRandom (randomR (x,y))
+        ps <- generateRandomInts (n-1) x y
+        return (p:ps)
+
+insertSetList :: [Int] -> Set Int -> Set Int
+insertSetList [] res = res
+insertSetList (x:xs) res = insertSetList xs (insertSet x res) 
+
+generateSetWithSize :: Int -> Int -> IO (Set Int)
+generateSetWithSize n max = do
+    let empty = emptySet
+    rands <- generateRandomInts n 0 max
+    let res = insertSetList rands empty
+    return res
+
+instance Arbitrary a => Arbitrary (Set a) where
+    arbitrary = liftM Set arbitrary
+
+randomSetTest :: Set Int -> Bool
+randomSetTest xs = isEmpty xs || (not . isEmpty) xs
+
+assignment2Quick = quickCheck randomSetTest
 
 {------------------------------------------------------------------------------
 
   Assignment 3
 
-  Hours spent: ?
+  Hours spent: 1
 
 ------------------------------------------------------------------------------}
 
+recurseIntersection :: Ord a => Set a -> Set a -> Set a -> Set a
+recurseIntersection (Set []) b res = res 
+recurseIntersection (Set (x:xs)) b res = if inSet x b 
+                            then recurseIntersection (Set xs) b (insertSet x res)
+                            else recurseIntersection (Set xs) b res 
+
+setIntersection :: Ord a => Set a -> Set a -> Set a
+setIntersection a b = recurseIntersection a b emptySet
+
+recurseUnion :: Ord a => Set a -> Set a -> Set a -> Set a
+recurseUnion (Set []) b res = res
+recurseUnion (Set (x:xs)) b res = recurseUnion (Set xs) b (insertSet x res)
+
+setUnion :: Ord a => Set a -> Set a -> Set a
+setUnion a b = recurseUnion a b emptySet
+
+recurseDifference :: Ord a => Set a -> Set a -> Set a -> Set a
+recurseDifference (Set []) b res = res
+recurseDifference (Set (x:xs)) b res = if inSet x b
+                            then recurseDifference (Set xs) b res
+                            else recurseDifference (Set xs) b (insertSet x res)
+
+setDifference :: Ord a => Set a -> Set a -> Set a
+setDifference a b = recurseDifference a b emptySet
+
+allSetList :: Ord a => (a -> Bool) -> Set a -> Bool
+allSetList f (Set lst) = all f lst 
+
+testIntersection :: Ord a => Set a -> Set a -> Bool
+testIntersection (Set a) (Set b) = allSetList (\ x -> x `elem` a && x `elem` b) temp
+    where temp = setIntersection (Set a) (Set b)
+
+testUnion :: Ord a => Set a -> Set a -> Bool
+testUnion (Set a) (Set b) = allSetList (\ x -> x `elem` a || x `elem` b) temp
+    where temp = setUnion (Set a) (Set b)
+
+testDifference :: Ord a => Set a -> Set a -> Bool
+testDifference (Set a) (Set b) = allSetList (\ x -> (x `elem` a) && (x `notElem` b)) temp
+    where temp = setDifference (Set a) (Set b)
+
+testIntIntersection :: [Int] -> [Int] -> Bool
+testIntIntersection a b = testIntersection (list2set a) (list2set b)
+testIntUnion :: [Int] -> [Int] -> Bool
+testIntUnion a b = testUnion (list2set a) (list2set b) 
+testIntDifference :: [Int] -> [Int] -> Bool
+testIntDifference a b = testDifference (list2set a) (list2set b)
+
+assignment3OwnGenerator = do 
+            setA <- generateSetWithSize 100 50
+            setB <- generateSetWithSize 100 50
+            print (testIntersection setA setB)
+            print (testIntersection setB setA)
+            print (testUnion setA setB)
+            print (testUnion setB setA)
+            print (testDifference setA setB)
+            print (testDifference setB setA)
+
+quickCheckIntersection = quickCheck testIntIntersection
+quickCheckUnion = quickCheck testIntUnion
+quickCheckDifference = quickCheck testIntDifference
 
 {------------------------------------------------------------------------------
 
@@ -118,14 +203,23 @@ unionSet (Set (x:xs)) set2  =
 
 ------------------------------------------------------------------------------}
 
+
+
 {------------------------------------------------------------------------------
 
   Assignment 5
 
-  Hours spent: ?
+  Hours spent: 0.5
 
 ------------------------------------------------------------------------------}
 
+type Rel a = [(a,a)]
+
+symClos :: Ord a => Rel a -> Rel a
+symClos = concatMap (\ (x,y) -> if x == y then [(x,y)] else [(x,y), (y, x)])
+
+testSymClos = symClos [(1,2), (2,3), (3,4)]
+testSymClosDouble = symClos [(1,1), (2,3), (4,3), (6,4)]
 
 {------------------------------------------------------------------------------
 
@@ -141,6 +235,19 @@ infixr 5 @@
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
+recurseTrClos :: Ord a => Rel a -> Rel a -> Rel a
+recurseTrClos [] full = []
+recurseTrClos x full = temp++(recurseTrClos temp full) where temp = x @@ full
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates [] = []
+removeDuplicates (x:xs) = x : removeDuplicates (filter (/=x) xs)
+
+trClos :: Ord a => Rel a -> Rel a
+trClos a = removeDuplicates (concatMap (\x -> x : recurseTrClos [x] a) a)
+
+testTrClos = trClos [(1,2),(2,3),(3,4)]
+testTrClos2 = trClos [(1,2),(1,4),(2,3),(3,4)]
 
 {------------------------------------------------------------------------------
 
