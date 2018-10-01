6268cfebed30bdec5ac2c9ea49ef7e25fd6a80cd
Thomas van der Ham <thomasvanderham@outlook.com>
2018-10-01 23:43:47 +0200

    Finished questions + extra output

--- Lab4/thomas/Lab4.hs
+++ Lab4/thomas/Lab4.hs
@@ -83,9 +83,10 @@ unionSet (Set (x:xs)) set2  =
 
   Assignment 1
 
-  Hours spent: ?
+  Hours spent: 2
   Answer:
-    - TODO
+    - What is a real world use case of a complement?
+    - Are there other ways to represent sets in Haskell which do not use lists?
 
 ------------------------------------------------------------------------------}
 
@@ -197,9 +198,13 @@ quickCheckDifference = quickCheck testIntDifference
 
   Assignment 4
 
-  Hours spent:
+  Hours spent: 3
   Answer:
-    - TODO
+    - Why is R = {(1,4),(1,5),(2,5)} is a relation from {1,2,3} to {4,5,6}
+      and not a relation from {1,2} to {4,5}?
+    - What is the function of the identity relation?
+    - Why are the functions curry and uncurry fundamental for functional
+      programming?
 
 ------------------------------------------------------------------------------}
 
@@ -220,6 +225,12 @@ symClos = concatMap (\ (x,y) -> if x == y then [(x,y)] else [(x,y), (y, x)])
 
 testSymClos = symClos [(1,2), (2,3), (3,4)]
 testSymClosDouble = symClos [(1,1), (2,3), (4,3), (6,4)]
+{-
+Output:
+[(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]
+[(1,1),(2,3),(3,2),(4,3),(3,4),(6,4),(4,6)]
+Which are correct
+-}
 
 {------------------------------------------------------------------------------
 
@@ -250,6 +261,13 @@ trClos a = removeDuplicates (concatMap (\x -> x : recurseTrClos [x] [x] a) a)
 testTrClos = trClos [(1,2),(2,3),(3,4)]
 testTrClos2 = trClos [(1,2),(1,4),(2,3),(3,4)]
 testTrClos3 = trClos [(1,2),(1,3),(1,4),(4,1)]
+{-
+Output:
+[(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
+[(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]
+[(1,2),(1,3),(1,4),(1,1),(4,1),(4,2),(4,3),(4,4)]
+Which are correct
+-}
 
 {------------------------------------------------------------------------------
 
@@ -283,7 +301,22 @@ quickCheckTransitivity = quickCheck testTransitive
 
   Assignment 8
 
-  Hours spent: ?
+  Hours spent: 0.3
+  There is a difference, because the order does matter. When pairs do not overlap
+  they are still transitive so the size of the set does not change, but the size
+  does change by using symmetry. Also after symmetry is added extra pairs have
+  to be added in order make the closure transitive.
+  
+  Examples:
+  sym -> trans
+  {(1,2),(3,4)} -> {(1,2),(2,1),(3,4),(4,3)} -> {(1,2),(1,1),(2,1),(2,2),(3,4),(3,3),(4,3),(4,4)}
 
+  trans -> sym
+  {(1,2),(3,4)} -> {(1,2),(3,4)} -> {(1,2),(2,1),(3,4),(4,3)}
+
+  This can also be checked by execution the following:
+  symClos (trClos [(1,2), (3,4)]) /= trClos (symClos [(1,2), (3,4)])
+  output:
+  True
 ------------------------------------------------------------------------------}
 
