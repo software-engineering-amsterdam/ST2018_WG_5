51ef8e4aebb42b9d92a204795574a23894328a38
Michael Peters <m.peters@m2mobi.com>
2018-10-02 19:49:14 +0200

    Merged the reading assignments

--- Lab4/final/Lab4.hs
+++ Lab4/final/Lab4.hs
@@ -9,9 +9,31 @@ import SetOrd
 
   Assignment 1
 
-  Hours spent: TODO
   Answer:
-    - TODO
+    - The Russel Paradox is not entirely clear to me.
+    - The halts function is not very clear to me.
+    - Page 136
+    - Notation in example 4.6 doesn’t really make the point clear for me
+    - Exercise 4.26 and 4.27. Coming up with proof (proof in general)
+    - What is a real world use case of a complement?
+    - Are there other ways to represent sets in Haskell which do not use lists?
+
+    -- Remark. The question whether the equation a = { a } has solutions (or, more
+    -- generally,whether sets a exist such that a ∈ a) is answered differentlyby different
+    -- axiomatizations of set theory.
+    --
+    -- What does this mean?
+
+    -- Page 137
+    -- Exercise 4.11 Explain that ∅ != {∅}
+    -- Is that because {∅} is not empty because it has an empty element?
+
+    -- Page 149
+    -- Exercise 4.40 1. Assume that A and B are non-empty and that A × B = B × A. Show that A = B.
+    -- 2. Show by means of an example that the condition of non-emptiness in 1 is necessary. (Did you use this in your proof of 1?)
+    --
+    -- I think i can solve this problem using a very simple case where A = B = {1,2}
+    -- But how can I verify that my simple example is enough to proof that non-emptiness in 1 is necessary
 
 ------------------------------------------------------------------------------}
 
@@ -41,7 +63,28 @@ import SetOrd
 
   Hours spent:
   Answer:
-    - TODO
+    - Why is R = {(1,4),(1,5),(2,5)} is a relation from {1,2,3} to {4,5,6}
+      and not a relation from {1,2} to {4,5}?
+    - What is the function of the identity relation?
+    - Why are the functions curry and uncurry fundamental for functional
+      programming?
+    - Again coming up with proof in general
+    - 5.63,5.64,6.65 is not clear for me
+    - Partitions. And especially such notation used at 5.83
+
+    -- Page 200
+    -- Example 5.63 The relation ∼ between vectors in 3-dimensional space R 3 that is
+    -- defined by~ a ∼ ~ b ≡ ∃r ∈ R + (~ a = r ~ b) is an equivalence.
+    --
+    -- What does the ∼ symbol mean between the two vectors? Never seen it before in this book
+
+    -- Page 213
+    -- Integer Partitions
+    -- I cannot figure out for what kind of problems integer partitions can be usefull (ignoring the cash change examples)
+
+    -- Page 204
+    -- Equivalence classes
+    -- I dont see why this needs to be a new class.
 
 ------------------------------------------------------------------------------}
 
