3f60d16bc523f7bfb9bcfb1da05c9ab9eeeda417
Michael Peters <m.peters@m2mobi.com>
2018-10-10 21:38:04 +0200

    Finished assignment 6.2

--- Lab6/michael/Assignment6.hs
+++ Lab6/michael/Assignment6.hs
@@ -48,6 +48,71 @@ assignment61 = do
 
   Hours spent:
   Answer:
-    TODO
+    For this excercise I will try to find the first 15 mersenne primes using the method described
+    in the exercise. Trying out k = 1 and k = 10 to see if that makes a difference.
+
+    A list found on the internet of the first 15 mersenne primes looks like this:
+    2,3,5,7,13,17,19,31,61,89,107,127,521,607,1279
+    (https://en.wikipedia.org/wiki/Mersenne_prime)
+
+    They seem to match with what I've found regardless of K. K only seems to have influences on the
+    execution time it takes to come up with the numbers
 
 ------------------------------------------------------------------------------}
+
+toMersenne x = (2^x) - 1
+mersenneIsMRPrime k x = primeMR k (toMersenne x)
+
+assignment62 0 k _ = print "done"
+assignment62 n k (x:xs) = do
+                    result <- mersenneIsMRPrime k x
+                    if result then do
+                                print (show x)
+                                assignment62 (n - 1) k xs
+                            else assignment62 n k xs
+
+assignment62k1 = do
+                    assignment62 15 1 primes
+{- Output:
+    *Assignment6> assignment62k1
+    "2"
+    "3"
+    "5"
+    "7"
+    "13"
+    "17"
+    "19"
+    "31"
+    "61"
+    "89"
+    "107"
+    "127"
+    "521"
+    "607"
+    "1279"
+    "done"
+    (0.28 secs, 213,981,528 bytes)
+-}
+
+assignment62k10 = do
+                    assignment62 15 10 primes
+{- Output:
+    *Assignment6> assignment62k10
+    "2"
+    "3"
+    "5"
+    "7"
+    "13"
+    "17"
+    "19"
+    "31"
+    "61"
+    "89"
+    "107"
+    "127"
+    "521"
+    "607"
+    "1279"
+    "done"
+    (0.47 secs, 356,854,320 bytes)
+-}
