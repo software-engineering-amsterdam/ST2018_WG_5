c89a89b9dc0c892b7e969d3f483ac6008c824ff7
Ewoudbouman <boumanewoud@gmail.com>
2018-10-14 20:15:13 +0200

    ouwe meuk moest nog weg

--- Lab6/ewoud/Lab6.hs
+++ Lab6/ewoud/Lab6.hs
@@ -327,14 +327,6 @@ A k = 2 is sufficient to discover the first 15 Mersenne primes reliably using
 the Miller-Rabin primality check
 ------------------------------------------------------------------------------}
 
--- List containing the first 47 Mersenne exponents
--- Source: https://oeis.org/A000043
-mersenneExp :: [Integer]
-mersenneExp = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 
-                4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243, 110503, 132049, 
-                216091, 756839, 859433, 1257787, 1398269, 2976221, 3021377, 6972593, 13466917, 
-                20996011, 24036583, 25964951, 30402457, 32582657, 37156667, 42643801, 43112609]
-
 -- Returns the first n mersenne numbers
 -- in a list.
 -- Goes upto mersenne number 25 (2^21701-1)
